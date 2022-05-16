7ab711bf88f81f39df6fbf3cb2124189436c3000
pam <pamela.palomita@gmail.com>
2022-05-15 22:16:10 -0700

    styled buttons

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Routes, Route, Switch} from "react-router-dom";
+import { Routes, Route, Switch, Link} from "react-router-dom";
 import GreetingContainer from "./greeting/greeting_container";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
@@ -8,7 +8,18 @@ import SignupFormContainer from "./session/signup_form_container";
 
 const App = () => (
   <div>
-    <h1>Welcome to my Co-opt</h1>
+    <div className="header">
+      <div className="inner-header">
+        <div className="inner-header-left">
+          <img className="logo" src="final-logo.png" />
+          <Link className='shop-btn' to='/underconstruction'>Shop</Link>
+        </div>
+        <div className="inner-header-right">
+          <Link className='stores-btn' to='/underconstruction'>Stores</Link>
+          <Link className='cart-btn' to='/underconstruction'>Cart</Link>
+        </div>
+      </div>
+    </div>
     {/* <ProtectedRoute element={<GreetingContainer/>} /> */}
     <Routes>
       <Route path="/login" element={<LoginFormContainer/>}   />
@@ -16,6 +27,7 @@ const App = () => (
       <Route path="/underconstruction" element={<h2>Sorry, this page is under construction</h2>}  />
       <Route path="/" element={<h1>SPLISH SPOLACHSD</h1>}  />
     </Routes>
+    <div className="footer"> How are we doing? Give us feedback on this page. </div>
   </div>  
 )
 
--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -22,36 +22,39 @@ class LoginForm extends React.Component {
   
   render(){
     return(
-      <div className="two-col-ctr">
-        <div className="session-form">
-          <h1>Your My Co-opt Online Account</h1>
-          <h3>Sign in or create an account.</h3>
-          <form onSubmit={this.handleSubmit}>
-            <label>Email
-              <input 
-                type="text"
-                value={this.state.email} 
-                onChange={this.handleChange('email')}
-              />
-            </label>
-            <label>Password
-              <input 
-                type="password"
-                value={this.state.password} 
-                onChange={this.handleChange('password')}
-              />
-            </label>
-            <Link to="/underconstruction">Forgot password?</Link>
-            <button className='grn-btn'>Sign in</button>
-          </form>
-          <ul>
-            {this.props.errors.session.map(error=> <li key={`error${this.props.error}`}>{error}</li>)}
-          </ul>
-        </div>
-        <div className="msg">
-          <h3>Don't have an account?</h3>
-          <p>A My Coopt Online Account lets you check out quicker, view your order status and history, add product reviews, and more - for the best online experience, attach your My Co-opt Membership to your account.</p>
-          <Link to="/signup">Create an account</Link>
+      <div>
+      <h1 className="h1">Your My Co-opt Online Account</h1>
+        <div className="two-col-ctr">
+          <div className="session-form">
+            
+            <h3>Sign in or create an account.</h3>
+            <form className="login-form" onSubmit={this.handleSubmit}>
+              <label>Email
+                <input 
+                  type="text"
+                  value={this.state.email} 
+                  onChange={this.handleChange('email')}
+                />
+              </label>
+              <label>Password
+                <input 
+                  type="password"
+                  value={this.state.password} 
+                  onChange={this.handleChange('password')}
+                />
+              </label>
+              <Link to="/underconstruction">Forgot password?</Link>
+              <button className='grn btn'>Sign in</button>
+            </form>
+            <ul>
+              {this.props.errors.session.map(error=> <li key={`error${this.props.error}`}>{error}</li>)}
+            </ul>
+          </div>
+          <div className="msg">
+            <h3>Don't have an account?</h3>
+            <p>A My Coopt Online Account lets you check out quicker, view your order status and history, add product reviews, and more - for the best online experience, attach your My Co-opt Membership to your account.</p>
+            <Link className='grn btn' to="/signup">Create an account</Link>
+          </div>
         </div>
       </div>
     )
